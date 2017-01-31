9357b1d21a3a838196fedaf69b05e9b0cee063bd
Ermyas Abebe <etabebe@au1.ibm.com>
2017-01-31 19:53:00 +1100

    Add checks for Tx success in benchmark script

--- examples/7nodes/bench-public-sync.sh
+++ examples/7nodes/bench-public-sync.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -u
 set -e
 # c = number of concurrent connections (sending transactions over and over)
@@ -9,5 +9,33 @@ set -e
 c=$1
 d=$2
 t=$3
-curl -d '' http://localhost:22000/
-wrk -s send-public-sync.lua -c $c -d $d -t $t http://localhost:22000/
+
+# Time to wait before checking for Tx receipts
+DELAY=10
+URL=http://localhost:22000/
+
+txListFile="txs-$c-$d-$t.out"
+failedTxFile="failed-txs-$c-$d-$t.out"
+
+# curl -d '' http://localhost:22000/
+wrk -s send-public-sync.lua -c $c -d $d -t $t $URL | sed -ne 's/.*\(0[xX][0-9a-fA-F]*\).*/\1/p' > $txListFile
+echo "Finished submitting transactions"
+echo -e "Sleeping for $DELAY seconds, before requesting transaction receipts..."
+sleep $DELAY
+
+echo -e "Requesting Tx receipts..."\\n
+failedCount=0
+totalChecked=0
+failedList=""
+while read -r tx; do
+	totalChecked=$(($totalChecked + 1))
+	result=`curl -s -d "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionReceipt\",\"params\":[\"$tx\"],\"id\":1}" $URL`
+	if [ `echo $result | grep -c 'blockNumber'` -eq 0 ]; then
+		failedCount=$(($failedCount + 1))
+		failedList="$tx\n$failedList"
+	fi
+	echo -n -e Failed: $failedCount, Total checked: $totalChecked Percentage failure: $((($failedCount * 100) / $totalChecked))% \\r
+done < $txListFile
+
+echo
+echo -e $failedList > $failedTxFile
